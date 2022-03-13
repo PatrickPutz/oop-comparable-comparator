6551c27ef046784ee6107e31a2b80d77f80e7464
patrick-putz <Putz.Patrick.1996@gmail.com>
2022-03-13 18:16:30 +0100

    Arrays - Car Array Sorting

--- src/org/campus02/arrays/ArrayHelper.java
+++ src/org/campus02/arrays/ArrayHelper.java
@@ -21,6 +21,27 @@ public class ArrayHelper {
         System.out.println("");
         System.out.println("");
 
+        Car vw1 = new Car("VW 1", 1998, 300000);
+        Car vw2 = new Car("VW 2", 1997, 400000);
+        Car bmw1 = new Car("BMW 1", 1996, 100000);
+        Car bmw2 = new Car("BMW 2", 1995, 80000);
+
+        Car[] carArray = {vw1, vw2, bmw1, bmw2};
+
+        System.out.println("<<< Initial Array >>>");
+        for(int i = 0; i < carArray.length; i++){
+            System.out.println(carArray[i].toString());
+        }
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("<<< Sorted Array >>>");
+        arrangeOrder(carArray);
+        for(int i = 0; i < carArray.length; i++){
+            System.out.println(carArray[i].toString());
+        }
+        System.out.println("");
+        System.out.println("");
     }
 
     public static void arrangeOrder(int[] numbers) {
@@ -35,4 +56,18 @@ public class ArrayHelper {
         }
 
     }
+
+    public static void arrangeOrder(Car[] cars) {
+
+        for(int i = 0; i < cars.length - 1; i++) {
+            if (cars[i].getProductionYear() > cars[i + 1].getProductionYear()) {
+                Car younger = cars[i + 1];
+                cars[i + 1] = cars[i];
+                cars[i] = younger;
+                arrangeOrder(cars);
+            }
+        }
+
+    }
+
 }
---
+++ src/org/campus02/arrays/Car.java
@@ -0,0 +1,47 @@
+package org.campus02.arrays;
+
+public class Car {
+
+    private String carType;
+    private int productionYear;
+    private int drivenKilometer;
+
+    public Car(String carType, int productionYear, int drivenKilometer) {
+        this.carType = carType;
+        this.productionYear = productionYear;
+        this.drivenKilometer = drivenKilometer;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public int getDrivenKilometer() {
+        return drivenKilometer;
+    }
+
+    public void setDrivenKilometer(int drivenKilometer) {
+        this.drivenKilometer = drivenKilometer;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carType='" + carType + '\'' +
+                ", productionYear=" + productionYear +
+                ", drivenKilometer=" + drivenKilometer +
+                '}';
+    }
+}
